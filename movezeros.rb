23a4b4a0a1e7cf2d158f2cce678d42b84bc1352e
Meshu <meshu102@outlook.com>
2023-01-04 10:58:18 +0300

    Solve the challenge

---
+++ movezeros.rb
@@ -0,0 +1,22 @@
+def move_zeroes(nums)
+ i = 0
+ j = 1
+
+  while j < nums.size 
+    if nums[i].zero? && nums[j] != 0
+      temp = nums[j]
+      nums[j] = nums[i]
+      nums[i] = temp
+      i += 1
+      j += 1
+    elsif nums[i]!=0 && nums[j]!= 0 
+      i += 1
+      j += 1
+    elsif nums[i]!=0 && nums[j]=0
+        i += 1
+      j += 1
+    else 
+    j += 1
+    end
+  end
+end
